1cc2dd18624cfb86483c55f7c95ecd9127c5156b
Naqeeb Ahmed <Naqeeb532@outlook.com>
2022-03-29 02:07:40 +0530

    fixed logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ElectroTech from "../../assets/img/farmlogo.png";
+import classes from "./Logo.css";
 
 const Logo = (props) => {
   return (
-    <div className='logos' onClick={props.click}>
+    <div className={classes.logos} onClick={props.click}>
       <img src={ElectroTech } alt="logo" width={'170px'}  />
     </div>
   )
